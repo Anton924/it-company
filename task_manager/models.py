ca8f6dbbd02161d735fa15d4ef56ab3359a8f2fb
Anton Yakovenko <yakovenkoanton2007@gmail.com>
2025-12-17 23:14:52 +0100

    feat: Add models to the application

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,59 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from it_company import settings
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.SET_DEFAULT,
+        default="Probation period",
+        related_name="workers"
+    )
+
+
+class Task(models.Model):
+
+    PRIORITY_CHOICES = (
+        ("CRITICAL", "Urgent"),
+        ("HIGH", "High"),
+        ("MEDIUM", "Medium"),
+        ("LOW", "Low")
+    )
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField()
+    priority = models.CharField(
+        max_length=255,
+        choices=PRIORITY_CHOICES, default="MEDIUM"
+    )
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE, related_name="tasks")
+    assignees = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="tasks")
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    team_lead = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, related_name="teams_team_lead")
+    workers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="teams")
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    tasks = models.ManyToManyField(Task, related_name="projects") # I have to decide to user many2many or foreignkey
+    teams = models.ManyToManyField(Team, related_name="projects")
