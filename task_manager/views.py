ae9fe0f76da19277352da2637c707e7c73954501
Anton Yakovenko <yakovenkoanton2007@gmail.com>
2025-12-28 22:35:47 +0200

    feat: Add index view for task manager statistics

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
+from django.http import HttpRequest
 from django.shortcuts import render
+from task_manager.models import Task, Project, Team
 
-# Create your views here.
+def index(request: HttpRequest):
+    total_tasks_in_process = Task.objects.filter(is_completed=False).count()
+    total_projects = Project.objects.count()
+    total_workers = get_user_model().objects.count()
+    total_teams = Team.objects.count()
+
+    context = {
+        "total_tasks_in_process": total_tasks_in_process,
+        "total_projects": total_projects,
+        "total_workers": total_workers,
+        "total_teams": total_teams
+    }
+
+    return render(request, template_name="index.html", context=context)
+
