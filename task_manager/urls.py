95ae14a30477da6318b0a5285dee948cf72ca648
Anton Yakovenko <yakovenkoanton2007@gmail.com>
2026-01-29 00:03:02 +0100

    feat: Add path for task update page

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from task_manager.views import index, TaskListView
+from task_manager.views import TaskUpdateView
 
 
 urlpatterns = [
     path("", index, name="index"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update")
 ]
 
 app_name = "task_manager"
