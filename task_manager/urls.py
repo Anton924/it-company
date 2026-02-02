a07de511e063c86e238a2f40261e88b58d5d013e
Anton Yakovenko <yakovenkoanton2007@gmail.com>
2026-02-03 00:13:12 +0100

    feat: Add urls for updating, deleting, creating tasks

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -6,7 +6,9 @@ from task_manager.views import TaskUpdateView
 urlpatterns = [
     path("", index, name="index"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
-    path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update")
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create")
 ]
 
 app_name = "task_manager"
