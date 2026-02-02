e3af55436c0e75ebb78eb30f8b8884dadac6a135
Anton Yakovenko <yakovenkoanton2007@gmail.com>
2026-02-03 00:14:34 +0100

    chore: Import TaskListView, TaskDeleteView, TaskCreateView

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from task_manager.views import index, TaskListView
+from task_manager.views import index, TaskListView, TaskDeleteView, TaskCreateView
 from task_manager.views import TaskUpdateView
 
 
