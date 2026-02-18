4cd98992cf947b8ee14049d591db7da595444ab1
Anton Yakovenko <yakovenkoanton2007@gmail.com>
2026-02-18 02:02:26 +0200

    feat: Move django-view-breadcrumbs to local apps and customize

---
+++ view_breadcrumbs/constants.py
@@ -0,0 +1,7 @@
+from django.utils.translation import gettext_lazy as _
+
+LIST_VIEW_SUFFIX = ("list")
+CREATE_VIEW_SUFFIX = ("create")
+UPDATE_VIEW_SUFFIX = ("update")
+DELETE_VIEW_SUFFIX = ("delete")
+DETAIL_VIEW_SUFFIX = ("detail")
