f63f79ff79bc2bbc1456b7a13b0402d863f25901
Anton Yakovenko <yakovenkoanton2007@gmail.com>
2026-02-21 01:45:57 +0200

    feat: Add for model Tag test for description blank state

--- task_manager/tests/test_models.py
+++ task_manager/tests/test_models.py
@@ -18,6 +18,9 @@ class TagModelTest(TestCase):
     def test_name_max_length(self):
         self.assertEqual(self.tag._meta.get_field("name").max_length, 255)
 
+    def test_description_blank_state(self):
+        self.assertTrue(self.tag._meta.get_field("description").blank)
+
     def test_object_name_is_name(self):
         self.assertEqual(str(self.tag), self.tag.name)
 
