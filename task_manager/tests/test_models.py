dfc0204db0788f10ee34e6c267960237fb559a44
Anton Yakovenko <yakovenkoanton2007@gmail.com>
2026-02-21 08:13:36 +0200

    feat: Add for model Worker tests

--- task_manager/tests/test_models.py
+++ task_manager/tests/test_models.py
@@ -1,7 +1,13 @@
+from django.db import models
 from django.test import TestCase
 
 
-from task_manager.models import Tag, TaskType, Position
+from task_manager.models import (
+    Tag,
+    TaskType,
+    Position,
+    Worker
+)
 
 
 class TagModelTest(TestCase):
@@ -54,3 +60,33 @@ class PositionTest(TestCase):
 
     def test_object_name_is_name(self):
         self.assertEqual(str(self.position), self.position.name)
+
+
+class WorkerTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.worker = Worker.objects.create(
+            first_name="Ivan",
+            last_name="Ivanov",
+            email="ivanivanow@gmail.com",
+            position=Position.objects.create(name="developer")
+        )
+
+    def test_position_label(self):
+        self.assertEqual(self.worker._meta.get_field("position").verbose_name, "position")
+
+    def test_position_foreign_key_model(self):
+        self.assertEqual(self.worker._meta.get_field("position").remote_field.model, Position)
+
+    def test_position_on_delete_and_null_state(self):
+        # Checking on_delete to be set to SET_NULL
+        self.assertEqual(self.worker._meta.get_field("position").remote_field.on_delete, models.SET_NULL)
+        # Checking so that field can store Null value
+        self.assertTrue(self.worker._meta.get_field("position").remote_field.null)
+
+    def test_position_related_name(self):
+        self.assertEqual(self.worker._meta.get_field("position").remote_field.related_name, "workers")
+
+    def test_object_name_is_first_name_and_last_name(self):
+        object_name = f"{self.worker.first_name} {self.worker.last_name}"
+        self.assertEqual(str(self.worker), object_name)
