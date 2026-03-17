02c9cabe6dba7e979aa733d2e6b2f7a1b0740815
Anton Yakovenko <yakovenkoanton2007@gmail.com>
2026-03-17 17:49:53 +0100

    feat: Add tests for TaskDeleteView

--- task_manager/tests/test_views.py
+++ task_manager/tests/test_views.py
@@ -281,6 +281,40 @@ class TaskDetailViewTest(LoginClientTestMixin, TaskObjectCreationMixin):
         self.assertTemplateUsed(response, "task_manager/task_detail.html")
 
 
+class TaskDeleteViewTest(LoginClientTestMixin, TaskObjectCreationMixin):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.task = Task.objects.create(
+            name="Task",
+            description="Need some time",
+            deadline=date(2026, 2, 25),
+            is_completed=False,
+            priority="LOW",
+            task_type=cls.task_type,
+            project=cls.project
+        )
+
+        cls.url = reverse("task_manager:task-delete", kwargs={"pk": cls.task.pk})
+
+    def test_view_url_exist_at_desired_location(self):
+        response = self.client.get(f"/tasks/{self.task.pk}/delete/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_correct_template_name(self):
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, "task_manager/task_confirm_delete.html")
+
+    def test_task_deletion_post(self):
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Task.objects.filter(name="Task").exists())
+
+
     def test_user_logged_out_redirection(self):
         response = self.client.get(reverse("task_manager:task-list"))
         self.assertRedirects(response, "/accounts/login/?next=/tasks/")
