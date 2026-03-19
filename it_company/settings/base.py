6091163848c76ce08140695a637c8080a9edefbb
Anton Yakovenko <yakovenkoanton2007@gmail.com>
2026-03-19 02:04:41 +0100

    feat: Add for development and production different settings with common base configurations file

--- it_company/settings/base.py
+++ it_company/settings/base.py
@@ -13,19 +13,14 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-^@(xi@a(*6l5!h+5)m$=*0cip_0my$)uqum8xzntox%dyw(hsg")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != False
-
-ALLOWED_HOSTS = ["127.0.0.1"]
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # Application definition
@@ -46,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -75,17 +71,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "it_company.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
 
 # Password validation
@@ -123,6 +108,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = "staticfiles/"
 
 AUTH_USER_MODEL = "task_manager.Worker"
 
